47c7e29ac24c0f67bb0ca090f3d9af3cdb66fc51
Afshin Teymoori <afshin_tm@yahoo.com>
2019-08-05 09:38:44 +1000

    Add

--- MicroServices/SqlDockerContainer.bat
+++ MicroServices/SqlDockerContainer.bat
@@ -1,2 +1,5 @@
 docker pull mcr.microsoft.com/mssql/server:2017-latest
 docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=M@nager1352" -p 1433:1433 --name sql1 -d mcr.microsoft.com/mssql/server:2017-latest
+REM these two command added but not tested.
+docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=M@nager1352" -p 1433:1433 -v c:/data:/host_data --name sql1 -d mcr.microsoft.com/mssql/server:2017-latest
+docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=M@nager1352" -p 1433:1433 -v c:/data:/var/opt/mssql/data/ --name sql1 -d mcr.microsoft.com/mssql/server:2017-latest
